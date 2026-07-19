225a1b72d153357a7e85a516c656cacd313a7ba8
Dinh Hoang Anh <hoanganhdinh.dev@gmail.com>
2026-07-19 20:37:49 +0700

    Refactor Exercise41: improve naming and readability

--- src/week2/Exercise41.java
+++ src/week2/Exercise41.java
@@ -9,46 +9,46 @@ public class Exercise41 {
         int numberOfHouseholds = scanner.nextInt();
 
         if (numberOfHouseholds <= 0) {
-            System.out.println("Invalid number.");
+            System.out.println("Invalid number of households.");
             scanner.close();
             return;
         }
         int currentHousehold = 1;
-        final int PRICE_LEVEL_1 = 1800;
-        final int PRICE_LEVEL_2 = 2300;
-        final int PRICE_LEVEL_3 = 3000;
-        int bill = 0;
-        int sumBill = 0;
+        final int PRICE_TIER_1 = 1800;
+        final int PRICE_TIER_2 = 2300;
+        final int PRICE_TIER_3 = 3000;
+        int bill;
+        int totalBill = 0;
         int highestBill = 0;
-        int countBillOver200K = 0;
-        final int billOver200K = 200000;
+        int countBillsAboveThreshold = 0;
+        final int ELECTRICITY_BILL_THRESHOLD = 200_000;
         while (currentHousehold <= numberOfHouseholds) {
-            System.out.print("Household " + currentHousehold + " electricity: ");
-            int kWH = scanner.nextInt();
-            if (kWH < 0) {
-                System.out.println("Invalid kwh.");
+            System.out.print("Household " + currentHousehold + " electricity usage: ");
+            int electricityUsage = scanner.nextInt();
+            if (electricityUsage < 0) {
+                System.out.println("Invalid electricity usage.");
             } else {
-                if (kWH <= 50) {
-                    bill = kWH * PRICE_LEVEL_1;
-                } else if (kWH <= 100) {
-                    bill = kWH * PRICE_LEVEL_2;
+                if (electricityUsage <= 50) {
+                    bill = electricityUsage * PRICE_TIER_1;
+                } else if (electricityUsage <= 100) {
+                    bill = electricityUsage * PRICE_TIER_2;
                 } else {
-                    bill = kWH * PRICE_LEVEL_3;
+                    bill = electricityUsage * PRICE_TIER_3;
                 }
-                sumBill += bill;
-                if (highestBill < bill) {
+                totalBill += bill;
+                if (bill > highestBill) {
                     highestBill = bill;
                 }
-                if (bill > billOver200K) {
-                    countBillOver200K++;
+                if (bill > ELECTRICITY_BILL_THRESHOLD) {
+                    countBillsAboveThreshold++;
                 }
                 currentHousehold++;
             }
         }
-        double average = (double) (sumBill) / numberOfHouseholds;
-        System.out.printf("Average bill: %.2f%n", average);
+        double averageBill = (double) (totalBill) / numberOfHouseholds;
+        System.out.printf("Average bill: %.2f%n", averageBill);
         System.out.printf("Highest bill: %d VND%n", highestBill);
-        System.out.println("Households over 200000 VND: " + countBillOver200K);
+        System.out.println("Households above " + ELECTRICITY_BILL_THRESHOLD + " VND: " + countBillsAboveThreshold);
         scanner.close();
     }
 }
