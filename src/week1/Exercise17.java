a4171a91b013da1b56b24591b24c6aba7a9996b0
Dinh Hoang Anh <hoanganhdinh.dev@gmail.com>
2026-07-22 15:22:34 +0700

    Add Exercise17: determine number sign

---
+++ src/week1/Exercise17.java
@@ -0,0 +1,19 @@
+package week1;
+
+import java.util.Scanner;
+
+public class Exercise17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = scanner.nextInt();
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+        scanner.close();
+    }
+}
