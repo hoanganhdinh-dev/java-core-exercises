fad4a357af971c3433a0dc6852c2aa8d7d3e4032
Dinh Hoang Anh <hoanganhdinh.dev@gmail.com>
2026-07-23 13:53:30 +0700

    Add Exercise31: print numbers from 1 to 10 using while loop

---
+++ src/week3/Exercise31.java
@@ -0,0 +1,11 @@
+package week3;
+
+public class Exercise31 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
